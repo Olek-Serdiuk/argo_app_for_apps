7aecf2f9c91be7bf92e18ef0982a3554516bdd1e
Olek <oleksandr.serduik@arelion.com>
2022-07-25 15:10:43 +0200

    1

--- platform/argo/argo.sh
+++ platform/argo/argo.sh
@@ -2,3 +2,6 @@
 kubectl create namespace argocd
 kubectl apply -n argocd -f install.yaml
 kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+kubectl apply -n argocd -f app_of_apps.yml
+
+
