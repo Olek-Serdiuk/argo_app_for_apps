6805c7092a5cb6ead5e14b70178a0cc5edee4e25
Olek <oleksandr.serduik@arelion.com>
2022-07-26 10:48:08 +0200

    testing

---
+++ bootstrap/argo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -ex
+# kubectl create namespace argocd
+kubectl apply -n argocd -f ../platform/argocd/install.yaml
+sleep 30
+kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+# kubectl apply -n argocd -f app_of_apps.yml
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
+kubectl port-forward svc/argocd-server -n argocd 8080:443
--- platform/argo/argo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-kubectl create namespace argocd
-kubectl apply -n argocd -f install.yaml
-kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
-kubectl apply -n argocd -f app_of_apps.yml
-
-
