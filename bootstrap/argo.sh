016b85ab8bb818005a106ca5a4ea322d2b21899a
Olek <oleksandr.serduik@arelion.com>
2022-07-26 16:02:07 +0200

    fix

--- bootstrap/argo.sh
+++ bootstrap/argo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
-# kubectl create namespace argocd
-kubectl apply -n argocd -f ../platform/argocd/install.yaml
+kubectl create namespace argocd
+kubectl apply -n argocd -f ../platform_plugins/argocd/install.yaml
 sleep 30
 kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
 # kubectl apply -n argocd -f app_of_apps.yml
