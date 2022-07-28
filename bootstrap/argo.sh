8b67f97c6d9bdfc0e7bfc31ff8dc3524febb3963
Olek <oleksandr.serduik@arelion.com>
2022-07-28 11:23:46 +0200

    test

--- bootstrap/argo.sh
+++ bootstrap/argo.sh
@@ -7,3 +7,6 @@ kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}
 # kubectl apply -n argocd -f app_of_apps.yml
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
 kubectl port-forward svc/argocd-server -n argocd 8080:443
+
+
+# kubectl argo rollouts promote demo-app-bg
